a1657048a366acf868b15f7ff5d9d8ae523b0c8a
Marcos Fraga <marcosjsfraga@gmail.com>
2020-07-15 07:55:33 -0300

    Implementing unit tests

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,36 @@
+import { uuid } from 'uuidv4';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+class FakeAppointmentsRepository implements IAppointmentsRepository {
+    private appointments: Appointment[] = [];
+
+    // Find appointment By Date
+    public async findByDate(date: Date): Promise<Appointment | undefined> {
+        const findAppointment = this.appointments.find(
+            appointment => appointment.date === date,
+        );
+
+        return findAppointment;
+    }
+
+    public async create({
+        provider_id,
+        date,
+    }: ICreateAppointmentDTO): Promise<Appointment> {
+        const appointment = new Appointment();
+
+        appointment.id = uuid();
+        appointment.date = date;
+        appointment.provider_id = provider_id;
+
+        this.appointments.push(appointment);
+
+        return appointment;
+    }
+}
+
+export default FakeAppointmentsRepository;
