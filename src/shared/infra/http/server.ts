a656931150fee094106a0a2983f79c51b4683bac
Marcos Fraga <marcosjsfraga@gmail.com>
2020-08-08 18:29:24 -0300

    Changes in Notifications

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -15,12 +15,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Middleware to print request route in terminal
+function logResquests(request: Request, response: Response, next: NextFunction) {
+    const { method, url } = request;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.log(`Request: ${logLabel}`);
+
+    return next();
+}
+app.use(logResquests);
+
 // Middleware to serve static files
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 // Middleware do handle errors
 app.use((error: Error, request: Request, response: Response, _: NextFunction) => {
+    console.log('-------- *** ERROR *** ----------');
+    console.log(error.message);
+    console.log('---------------------------------');
+
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
             status: 'error',
